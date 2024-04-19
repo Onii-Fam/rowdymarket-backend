e5023b3c67889d4422adf2347b85611be5476884
Alfonso <hipstercatz@gmail.com>
2024-04-19 00:44:29 -0500

    add webconfig.java

--- website_db/Rowdyback/src/main/java/com/example/Rowdyback/CorsConfig.java
+++ website_db/Rowdyback/src/main/java/com/example/Rowdyback/CorsConfig.java
@@ -5,21 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // This will apply CORS to all your endpoints
+                .allowedOrigins("http://localhost:5173")  // Specify the URL of your frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
+                .allowedHeaders("*")  // Allowed headers
+                .allowCredentials(true);  // If you need to send cookies or authorization headers
     }
 }
