90f6ab58b1a3b01123dd921b61f394b5b5c7bd18
Alfonso <hipstercatz@gmail.com>
2024-04-22 12:09:27 -0500

    added a return of user id.

--- website_db/Rowdyback/src/main/java/com/example/Rowdyback/controller/LoginController.java
+++ website_db/Rowdyback/src/main/java/com/example/Rowdyback/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -25,16 +26,14 @@ public class LoginController {
 
         User foundUser = foundUserOptional.get();
 
-        // Assuming you have a method to check passwords that returns a boolean
         if (!checkPassword(user.getPassword(), foundUser.getPassword())) {
             return ResponseEntity.badRequest().body("Error: Incorrect password");
         }
 
-        return ResponseEntity.ok().body("User logged in successfully");
+        return ResponseEntity.ok(Map.of("message", "User logged in successfully", "userId", foundUser.getUserId()));
     }
 
     private boolean checkPassword(String providedPassword, String storedPassword) {
-        // This should be replaced with a proper password check, possibly using BCrypt or another secure method
         return providedPassword.equals(storedPassword);
     }
 }
